a59f4f5f794108671f7ae533d4e15713b084a641
WidowMaker110 <alexander.miller110@gmail.com>
2016-03-04 13:55:14 -0500

    Can now upload photos
    Set it up to handle uploading images to the screen. Now I will be
    setting up a listener for changes in the <img> elements to create the
    composite

--- index.js
+++ index.js
@@ -49,4 +49,23 @@ $(document).ready(function () {
 	    img.src = src;
 		return img;
 	}
+	
+    function readURL(input, target) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $(target).attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    $("#file").change(function(){
+        readURL(this, "#Image_1");
+    });
+	$("#file2").change(function(){
+		readURL(this, "#Image_2");
+	});
 });

01f6626173f62be5e1dbc5f89335514f0b8dc810
Alex <alexandermiller_2016@depauw.edu>
2016-03-11 17:58:37 -0500

    working on storing filter images locally
    need to put this all on a server

--- js/index.js
+++ js/index.js
@@ -176,6 +176,26 @@ $(document).ready(function () {
 	base_image = new Image();
 	base_image.src = 'grey-07.jpg';
 	base_image.onload = function(){
-    ctx.drawImage(base_image, 0, 0);
-  }
+		ctx.drawImage(base_image, 0, 0);
+	}
+	
+	var fileExt = {};
+    fileExt[0]=".png";
+    fileExt[1]=".jpg";
+    fileExt[2]=".gif";
+		
+	$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: 'C:/Users/alexa/Documents/GitHub/ImageOverlay/filter_images/',
+    success: function (data) {
+       $("#fileNames").html('<ul>');
+       //List all png or jpg or gif file names in the page
+       $(data).find('a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + ")').each(function () {
+           var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+           $("#fileNames").append( '<li>'+filename+'</li>');
+       });
+       $("#fileNames").append('</ul>');
+     }     
+  });
+	
 });
